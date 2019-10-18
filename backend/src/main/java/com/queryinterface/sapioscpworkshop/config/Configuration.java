337050ac6a3a82fc08fae43ed0df1318ac8ebde9
i051413 <arnaud.develay@sap.com>
2019-10-18 16:19:26 +0200

    add log configuration file

--- backend/src/main/java/com/queryinterface/sapioscpworkshop/config/Configuration.java
+++ backend/src/main/java/com/queryinterface/sapioscpworkshop/config/Configuration.java
@@ -2,15 +2,23 @@ package com.queryinterface.sapioscpworkshop.config;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Configuration {
 	
 	private static final String API_KEY = "API_KEY";
+
+	Logger logger = LoggerFactory.getLogger(Configuration.class);
 	
 	public Optional<String> getApiKey() {
 		try {
 			String apiKey = System.getenv(API_KEY);
 			return Optional.of(apiKey);
 		} catch (Exception e) {
+			if (logger.isErrorEnabled()) {
+				logger.error("Unable to read environment variable 'API_KEY'", e);
+			}
 			return Optional.empty();
 		}
 	}
